9d78bb7b855796c9d093fbd68e53bd2345fa6780
Huy39Quoc <167945869+Huy39Quoc@users.noreply.github.com>
2024-11-21 19:36:23 +0700

    Add files via upload

---
+++ OnlineMovieRentalSystem/src/Login/Main.java
@@ -0,0 +1,33 @@
+package Login;
+
+import java.sql.SQLException;
+import JDBC.ConnectJDBC;
+
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        InputMain input = new InputMain();
+        ConnectJDBC.init();
+        System.out.println("Hello, Welcome to our movie booking's website.");
+        boolean loop = true;
+        while(loop){
+        String haveAccount = input.InputString("Do you have an account yet?[Y/N]: ").toUpperCase();
+        switch(haveAccount){
+            case "Y":
+                loop = false;
+                Login login = new Login();
+                login.toLogin();
+                break;
+                
+            case "N":
+                loop = false;
+                CreateAccount acc = new CreateAccount();
+                acc.Account();
+                break;
+                
+            default:
+                System.out.println("Please only enter 'Y' or 'N'");
+        }
+       }
+    }
+}
